429e902d2ce0f2eb62a4f460776233aea809fb23
Yonatan Koritny <jkoritny@cs.huji.ac.il>
2022-07-29 02:21:01 +0300

    MADE MUCH MORE BEAUTIFUL

--- main.py
+++ main.py
@@ -1,28 +1,122 @@
-from multiprocessing import freeze_support
+"""
 
-freeze_support()
-import cv2
-from ffpyplayer.player import MediaPlayer
-from moviepy.video.compositing.concatenate import concatenate_videoclips
-from os import listdir
-import Fo
-from multiprocessing import Process
+An experiment.
+Can a computer generate a movie from random youtube videos and it will still have a narrative?
+
+"""
+
 import os
 import os.path
+import shutil
+import sys
+from math import ceil
+from multiprocessing import Pool
+from multiprocessing import Process
+from multiprocessing import freeze_support
+from os import listdir
+from random import uniform
+from subprocess import TimeoutExpired
+from time import sleep
 
+import ffmpeg
+from dotenv import load_dotenv
+from moviepy.video.compositing.concatenate import concatenate_videoclips
+from moviepy.video.fx.resize import resize
 from moviepy.video.io.VideoFileClip import VideoFileClip
-from constants import *
+from pafy import pafy
+from tqdm import tqdm
+
+import youtube_singleton
+
+MASTER_DOWNLOAD_TIMEOUT = 5 * 60
+
+OUTPUT_DIRECOTRY = "output"
+FPS = 25
+VIDEO_SIZE = (1920, 1080)
+MAX_BYTE_SIZE = 255
+MINUTES = 1
+SECONDS = 30
+TOTAL_TIME = MINUTES * 60 + SECONDS
+TEMP_DIRECTORY = OUTPUT_DIRECOTRY + "\\temp"
+CONVERTED_DIRECTORY = OUTPUT_DIRECOTRY + "\\conversion"
 
 
-def main():
-    video_from_internet_method = Process(target=Fo.make_temp_videos, daemon=False)
-    video_from_internet_method.start()
-    video_from_internet_method.join()
-    video_from_internet_method.terminate()
+def choose_video_time(elapsed_percentage):
+    if 0 <= elapsed_percentage <= 60:
+        time = -0.2 * elapsed_percentage + 15
+        time = max(0.04, time)
+    elif 60 < elapsed_percentage <= 80:
+        time = uniform(0.04, 0.12)
+    else:
+        time = quarter(uniform(5, 7))
+    elapsed_percentage += time * 100 / TOTAL_TIME
+    return time, elapsed_percentage
 
-    print("Finished downloading videos! Making master video")
-    concatenate(CONVERTED_DIRECTORY, OUTPUT_DIRECOTRY + '\\final' + str(
-        len([name for name in os.listdir(OUTPUT_DIRECOTRY)])) + '.mp4')
+
+def quarter(x):
+    return ceil(x * 4) / 4
+
+
+def progress_bar(i, j):
+    sleep(0.1)
+    sys.stdout.write('\r')
+    sys.stdout.write("Downloading queue: " + str(i) + " which are: " + str(j) + ' of the video')
+    sys.stdout.flush()
+
+
+def start_sub_process(file_name, out_filename, video_length):
+    """
+
+    :param file_name:
+    :param out_filename:
+    :param video_length:
+    :return:
+    """
+    retry_count = 0
+    while retry_count >= 10:
+        try:
+            if not get_video_from_youtube(out_filename, video_length):
+                retry_count += 1
+                continue
+            convert_video_file(file_name, out_filename)
+            check_file_integrity(file_name)
+        except Exception as e:  # TODO check for type of exceptions!!!!
+            # print(e, "\n ....................................Retrying!")
+            retry_count += 1
+            continue
+
+
+def check_file_integrity(file_name):
+    file = open(CONVERTED_DIRECTORY + '\\' + file_name, 'r')
+    file.close()
+
+
+def convert_video_file(file_name, out_filename):
+    clip = VideoFileClip(out_filename)
+    clip = resize(clip, height=1080)
+    clip.write_videofile(CONVERTED_DIRECTORY + '\\' + file_name, verbose=False, logger=None)
+    clip.close()
+
+
+def get_video_from_youtube(out_filename, time_of_video):
+    youtube_url = "https://www.youtube.com/watch?v=" + youtube_singleton.YouTubeSingleton().youtube_search()
+    stream_url = pafy.new(youtube_url).getbest().url
+    download_video_from_youtube = (
+        ffmpeg.input(stream_url, t=time_of_video).output(out_filename, f='mp4', acodec='aac', vcodec='libx264',
+                                                         loglevel="quiet").overwrite_output().run_async())
+    try:
+        download_video_from_youtube.wait(max(20, time_of_video * 3))
+        file = open(out_filename, 'r')
+        file.close()
+        return True
+    except TimeoutExpired:
+        # print("Timeout " + out_filename)
+        return False
+    except FileNotFoundError:
+        return False
+
+
+OPENING_SCRIPT = 'Making A Very Coherent Video'
 
 
 def concatenate(video_clips_path, output_path):
@@ -38,29 +132,77 @@ def concatenate(video_clips_path, output_path):
     final_clip = concatenate_videoclips(clips, method="compose")
     # write the output video file
     final_clip.write_videofile(output_path, codec='libx264')
-    PlayVideo(output_path)
 
 
-def PlayVideo(video_path):
-    video = cv2.VideoCapture(video_path)
-    player = MediaPlayer(video_path)
-    while True:
-        grabbed, frame = video.read()
-        audio_frame, val = player.get_frame()
-        if not grabbed:
-            print("End of video")
-            break
-        if cv2.waitKey(28) & 0xFF == ord("q"):
-            break
-        cv2.imshow("Video", frame)
-        if val != 'eof' and audio_frame is not None:
-            # audio
-            img, t = audio_frame
-    video.release()
-    cv2.destroyAllWindows()
+def download_youtube_videos():
+    create_output_dir()
+    with Pool(processes=8) as pool:
+        working_jobs, num_of_video = create_video_download_jobs(pool)
+        print('\n')
+        sys.stdout.flush()
+        download_progress_bar(num_of_video, working_jobs)
+        pool.terminate()
+        pool.join()
+
+
+def download_progress_bar(num_of_video, working_jobs):
+    num_of_finished_jobs = 0
+    sec_count = 0
+    with tqdm(total=num_of_video, desc="Number of videos downloaded") as pbar:
+        while True:
+            if sec_count >= MASTER_DOWNLOAD_TIMEOUT:
+                print("Terminating because it is taking too long. Making video now")
+                break
+            finished_jobs = [job for job in working_jobs if job.ready()]
+            num_of_finished_len = len(finished_jobs)
+            pbar.update(num_of_finished_len - num_of_finished_jobs)
+            num_of_finished_jobs = num_of_finished_len
+            if len(working_jobs) == num_of_finished_jobs:
+                print("finished downloading all videos!")
+                break
+            sleep(1)
+            sec_count = +1
+
+
+def create_video_download_jobs(pool):
+    num_of_video = 0
+    total_per = 0
+    working_jobs = []
+    while total_per < 100:
+        file_name = str(num_of_video) + ".mp4"
+        video_length, total_per = choose_video_time(total_per)
+        temp_filename = TEMP_DIRECTORY + "\\" + file_name
+        working_jobs.append(pool.apply_async(start_sub_process, (file_name, temp_filename, video_length)))
+        num_of_video += 1
+        progress_bar(num_of_video, total_per)
+    return working_jobs, num_of_video
+
+
+def create_output_dir():
+    if os.path.exists(TEMP_DIRECTORY):
+        shutil.rmtree(TEMP_DIRECTORY)
+    os.makedirs(TEMP_DIRECTORY)
+    if os.path.exists(CONVERTED_DIRECTORY):
+        shutil.rmtree(CONVERTED_DIRECTORY)
+    os.makedirs(CONVERTED_DIRECTORY)
+
+
+def download_videos():
+    video_from_internet_method = Process(target=download_youtube_videos, daemon=False)
+    video_from_internet_method.start()
+    video_from_internet_method.join()
+    video_from_internet_method.terminate()
+    print("Finished downloading videos! Making master video")
 
 
 if __name__ == '__main__':
-    freeze_support()
-    print('Making A Very Coherent Video')
-    main()
+    freeze_support()  # used for multiprocessing
+    load_dotenv()  # loading API key
+    print(OPENING_SCRIPT)
+    download_videos()
+    concatenate(CONVERTED_DIRECTORY, OUTPUT_DIRECOTRY + '\\final' + str(
+        len([name for name in os.listdir(OUTPUT_DIRECOTRY)])) + '.mp4')
+    print('\n \n  FINISHED! \n \n')
+    input('WRITE ME A STORY TO FINISH AND PRESS ENTER: ')
+    path = os.path.realpath(OUTPUT_DIRECOTRY)
+    os.startfile(path)
--- youtube_singleton.py
+++ youtube_singleton.py
@@ -1,10 +1,9 @@
+import os
 import random
 from threading import Lock
 
-import googleapiclient.errors
 from googleapiclient.discovery import build
 
-DEVELOPER_KEY = 'AIzaSyDSykfiYC_Q2FcCGORdBs3QuV9DshT_MDc'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
@@ -20,27 +19,9 @@ class YouTubeSingletonMeta(type):
     _instances = {}
 
     _lock: Lock = Lock()
-    """
-    We now have a lock object that will be used to synchronize threads during
-    first access to the Singleton.
-    """
 
     def __call__(cls, *args, **kwargs):
-        """
-        Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
-        """
-        # Now, imagine that the program has just been launched. Since there's no
-        # Singleton instance yet, multiple threads can simultaneously pass the
-        # previous conditional and reach this point almost at the same time. The
-        # first of them will acquire lock and will proceed further, while the
-        # rest will wait here.
         with cls._lock:
-            # The first thread to acquire the lock, reaches this conditional,
-            # goes inside and creates the Singleton instance. Once it leaves the
-            # lock block, a thread that might have been waiting for the lock
-            # release may then enter this section. But since the Singleton field
-            # is already initialized, the thread won't create a new object.
             if cls not in cls._instances:
                 instance = super().__call__(*args, **kwargs)
                 cls._instances[cls] = instance
@@ -48,22 +29,14 @@ class YouTubeSingletonMeta(type):
 
 
 class YouTubeSingleton(metaclass=YouTubeSingletonMeta):
-    """
-    We'll use this property to prove that our Singleton really works.
-    """
-
     def __init__(self) -> None:
-        self.youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+        self.youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=os.getenv('DEVELOPER_KEY'))
         self.video_mutex = Lock()
         self.video_mutex.acquire()
         self.videos = []
-        with open("video.txt", 'r') as fp:
+        with open("youtube_video_database.txt", 'r') as fp:
             for line in fp:
-                # remove linebreak from a current name
-                # linebreak is the last character of each line
                 x = line[:-1]
-
-                # add current item to the list
                 self.videos.append(x)
         self.video_mutex.release()
 
@@ -82,17 +55,13 @@ class YouTubeSingleton(metaclass=YouTubeSingletonMeta):
 
             self.video_mutex.acquire()
             # open file in write mode
-            with open("video.txt", 'a') as fp:
+            with open("youtube_video_database.txt", 'a') as fp:
                 for video in videos:
                     # write each item on a new line
                     fp.write("%s\n" % video)
             self.video_mutex.release()
-        #
-        # except googleapiclient.errors.HttpError:
-        #     # open file and read the content in a list
-        #     videos = self.videos
 
-        except Exception as e:
+        except Exception as e:  # TODO Add more precise exceptions!
             videos = self.videos
 
         return videos[random.randint(0, len(videos) - 1)]
